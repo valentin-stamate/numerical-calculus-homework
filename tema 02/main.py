9ad8e24834a46da9073ff3ca409718577e5c3889
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2022-03-07 19:48:03 +0200

    Finished homework 2

--- tema 02/main.py
+++ tema 02/main.py
@@ -59,13 +59,13 @@ def solve_triangular_matrix(a, n):
     return x
 
 
-def search_pivot(a, l, n):
+def search_pivot(a, col, n):
     index = 0
     m = -9999999
 
-    for i in range(l, n):
-        if abs(a[i, l]) > m:
-            m = abs(a[i, l])
+    for i in range(col, n):
+        if abs(a[i, col]) > m:
+            m = abs(a[i, col])
             index = i
 
     return index
@@ -76,74 +76,79 @@ def swap_lines(a, i, j):
 
 
 def gauss_elimination(a, n, a_init, b_init):
-    for l in range(0, n - 1):
-        pivot = search_pivot(a, l, n)
-        if pivot != l:
-            swap_lines(a, l, pivot)
+    for col in range(0, n - 1):
+        pivot = search_pivot(a, col, n)
+        if pivot != col:
+            swap_lines(a, col, pivot)
 
-        if abs(a[l, l]) < eps:
-            print('Nu se poate face impartirea')
+        if abs(a[col, col]) < eps:
+            print('Division not possible')
             break
 
-        for i in range(l + 1, n):
-            factor = - a[i, l] / a[l, l]
+        for i in range(col + 1, n):
+            factor = - a[i, col] / a[col, col]
 
             for j in range(0, n + 1):
-                a[i, j] += factor * a[l, j]
+                a[i, j] += factor * a[col, j]
 
-    l = n - 1
-    if abs(a[l, l]) <= eps:
-        print("Matrice singulara")
+    col = n - 1
+    if abs(a[col, col]) <= eps:
+        print("Singular matrix")
     else:
         x = solve_triangular_matrix(a, n)
-        norm = verify(a_init, x, b_init, n)
-
-        print("The norm is:")
-        print(norm)
-
         return x
 
     return None
 
 
-def gauss_elimination_old(a, n):
-    x = np.zeros(n)
+def extend_matrix(a, n):
+    i = np.zeros((n, 2 * n))
+    for line in range(n):
+        i[line, n + line] = 1
+    i[0:n, 0:n] = a
+    return i
 
-    for i in range(n):
-        if abs(a[i][i]) < eps:
-            print('Divide by zero detected!')
-            print('Singular matrix')
+
+def gauss_elimination_extended(a, n):
+    a = extend_matrix(a, n)
+    for col in range(0, n - 1):
+        pivot = search_pivot(a, col, n)
+        if pivot != col:
+            swap_lines(a, col, pivot)
+
+        if abs(a[col, col]) < eps:
+            print('Division not possible')
             break
 
-        for j in range(i + 1, n):
-            ratio = a[j][i] / a[i][i]
+        for i in range(col + 1, n):
+            factor = - a[i, col] / a[col, col]
 
-            for k in range(n + 1):
-                a[j][k] = a[j][k] - ratio * a[i][k]
+            for j in range(0, 2 * n):
+                a[i, j] += factor * a[col, j]
 
-    x[n - 1] = a[n - 1][n] / a[n - 1][n - 1]
+    col = n - 1
+    if abs(a[col, col]) <= eps:
+        print("Singular matrix")
+    else:
+        inverse_a = np.zeros((n, n))
+        for col in range(n):
+            a = swap_columns(a, n, n + col)
+            x = solve_triangular_matrix(a, n)
+            inverse_a[0:n, col] = x
+            a = swap_columns(a, n, n + col)
 
-    for i in range(n - 2, -1, -1):
-        x[i] = a[i][n]
+        return inverse_a
 
-        for j in range(i + 1, n):
-            x[i] = x[i] - a[i][j] * x[j]
+    return None
 
-        x[i] = x[i] / a[i][i]
 
-    save_matrix(a, x, n)
-
-    print_matrix(x, "The solution is")
-
-    solve_triangular_matrix(a, n)
-    return x
+def swap_columns(a, i, j):
+    a[:, [i, j]] = a[:, [j, i]]
+    return a
 
 
 def verify(a_init, x, b_init, n):
     b_cal = np.dot(a_init, x)
-    print_matrix(b_cal, "B Calc:")
-    print_matrix(b_init, "B Init")
-
     x_calc = b_cal - b_init
 
     s = 0
@@ -159,22 +164,31 @@ def main():
         [-3, -1, 2, -11],
         [-2, 1, 2, -3]
     ], dtype='float32')
-
-    n = 10
+    n = 100
     a = np.random.rand(n, n + 1)
 
     a_init = np.copy(a)
     b_init = a_init[:, -1]
 
+    # Point 1
     a_init = np.delete(a, axis=1, obj=n)
     x_gauss = gauss_elimination(a, n, a_init, b_init)
+    # print("X_gauss = ", end='')
+    # print(x_gauss)
+    # point 2
+    print("||A_init * X_gauss - b_init|| = " + str(verify(a_init, x_gauss, b_init, n)))
 
     # Point 3
     x_bibl = np.linalg.solve(a_init, b_init)
-    print(np.linalg.norm(x_gauss - x_bibl))
+    print("||X_gauss - X_bibl|| = " + str(np.linalg.norm(x_gauss - x_bibl)))
 
     a_ = np.linalg.inv(a_init).dot(b_init)
-    print(np.linalg.norm(x_gauss - a_))
+    print("||X_gauss - A^(-1)_bibl * b_init|| = " + str(np.linalg.norm(x_gauss - a_)))
+    # Point 4
+
+    a = np.copy(a_init)
+    gauss_inverse = gauss_elimination_extended(a, n)
+    print("||A^(-1)_gauss - A^(-1)_bibl|| = " + str(np.linalg.norm(np.linalg.inv(a_init) - gauss_inverse)))
 
 
 if __name__ == '__main__':
