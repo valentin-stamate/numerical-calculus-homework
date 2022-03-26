373a6dc9cc14000cfddcb9d91396c288865e2cce
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2022-03-26 12:51:51 +0200

    read matrix from files

--- tema 04/main.py
+++ tema 04/main.py
@@ -1,6 +1,36 @@
+def read_matrix(path):
+    with open(path) as f:
+        lines = f.readlines()
+
+    n = int(lines[0])
+    lines = lines[2:]
+
+    mp = [{} for _ in range(n)]
+
+    for line in lines:
+        val, i, j = line.split(',')
+        val = float(val.strip())
+        i = int(i.strip())
+        j = int(j.strip())
+
+        if mp[i].get(j) is not None:
+            mp[i][j] += val
+            continue
+
+        mp[i][j] = val
+
+    return n, mp
+
+
 def main():
-    pass
+    n, a1 = read_matrix('a_1.txt')
+    _, a2 = read_matrix('a_1.txt')
+    _, a3 = read_matrix('a_1.txt')
+    _, a4 = read_matrix('a_1.txt')
+    _, a5 = read_matrix('a_1.txt')
+
+    print(n)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
